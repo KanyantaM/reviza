3a2c0279f982d035a73d2d8838e815e32ae687a4
KanyantaM <kanyanta.1makasa@gmail.com>
2024-02-14 15:27:42 +0200

    removing unnessecary AI screens

--- lib/features/ai_chat_screen/bard_chat_screen.dart
+++ lib/features/ai_chat_screen/bard_chat_screen.dart
@@ -1,11 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:reviza/features/ai_chat_screen/sections/chat.dart';
-import 'package:reviza/features/ai_chat_screen/sections/embed_batch_contents.dart';
-import 'package:reviza/features/ai_chat_screen/sections/embed_content.dart';
-import 'package:reviza/features/ai_chat_screen/sections/response_widget_stream.dart';
-import 'package:reviza/features/ai_chat_screen/sections/stream.dart';
-import 'package:reviza/features/ai_chat_screen/sections/text_and_image.dart';
-import 'package:reviza/features/ai_chat_screen/sections/text_only.dart';
 
 class SectionItem {
   final int index;
@@ -26,13 +20,7 @@ class _MyHomePageState extends State<AiScreen> {
   final int _selectedItem = 2;
 
   final _sections = <SectionItem>[
-    SectionItem(0, 'Stream text', const SectionTextStreamInput()),
-    SectionItem(1, 'textAndImage', const SectionTextAndImageInput()),
     SectionItem(2, 'chat', const SectionChat()),
-    SectionItem(3, 'text', const SectionTextInput()),
-    SectionItem(4, 'embedContent', const SectionEmbedContent()),
-    SectionItem(5, 'batchEmbedContents', const SectionBatchEmbedContents()),
-    SectionItem(6, 'response without setState()', const ResponseWidgetSection()),
   ];
 
   @override

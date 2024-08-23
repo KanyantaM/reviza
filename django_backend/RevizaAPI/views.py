498a4de962f7d1044a39a4bec7e2ab67c4a268bb
KanyantaM <kanyanta.1makasa@gmail.com>
2024-08-23 09:32:57 +0200

    creating the views and linking them with the urls of the main and app folders

--- django_backend/RevizaAPI/views.py
+++ django_backend/RevizaAPI/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 # Create your views here.
+
+@api_view()
+def studyMaterial(request):
+    return Response('List of the study materials', status = status.HTTP_200_OK)

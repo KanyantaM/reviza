b556221c4b036ed47aeb32cd3804646918f7f30b
KanyantaM <kanyanta.1makasa@gmail.com>
2024-08-23 12:25:06 +0200

    trying out view classes

--- django_backend/RevizaAPI/urls.py
+++ django_backend/RevizaAPI/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('studyMaterials/', views.studyMaterial)
+    # path('studyMaterials/', views.studyMaterial)
+    path('studyMateriels/', views.StudyMaterials.as_view())
 ]

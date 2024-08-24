3197f718b7d3bceb4dbb41ff5329b3a8d4a8a54b
kangwa623 <kangwamuk3020@gmail.com>
2024-08-24 22:18:11 +0200

    side bar changes and tiles.

--- web_front_end/static/bts-5/js/dashboardScript.js
+++ web_front_end/static/bts-5/js/dashboardScript.js
@@ -1,37 +1,7 @@
 /* file: dashboardScript.js */
 
-// Import Firebase SDKs and configure Firebase (similar to testfile.html)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
-import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+const hamBurger = document.querySelector(".toggle-btn");
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyB-HOo4dN79-d0Z1e3sWwuF-UGvlSmhcMc",
-    authDomain: "testfile-1aec5.firebaseapp.com",
-    projectId: "testfile-1aec5",
-    storageBucket: "testfile-1aec5.appspot.com",
-    messagingSenderId: "103563495546",
-    appId: "1:103563495546:web:382b619c7e501f1e6053c3"
-};
-
-// Initialize Firebase (similar to testfile.html)
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-const signOutButton = document.querySelector("#signOutButton");
-
-const userSignOut = async () => {
-    await signOut(auth)
-        .then(() => {
-            // Redirect back to the login page after signing out
-            alert("You are now signed out!");
-            window.location.href = "/";
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorCode, errorMessage);
-        });
-}
-
-signOutButton.addEventListener("click", userSignOut);
+hamBurger.addEventListener("click", function () {
+  document.querySelector("#sidebar").classList.toggle("expand");
+});

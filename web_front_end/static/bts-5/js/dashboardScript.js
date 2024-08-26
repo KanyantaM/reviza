58b92d46be5cb3e902cf7228eb45ade7651b14eb
kangwa623 <kangwamuk3020@gmail.com>
2024-08-26 03:03:07 +0200

    side-bar and main dashboard frame.

--- web_front_end/static/bts-5/js/dashboardScript.js
+++ web_front_end/static/bts-5/js/dashboardScript.js
@@ -1,7 +1,42 @@
 /* file: dashboardScript.js */
 
 const hamBurger = document.querySelector(".toggle-btn");
+var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+var cardWidth = $(".carousel-item").width();
+var scrollPosition = 0;
+
 
 hamBurger.addEventListener("click", function () {
   document.querySelector("#sidebar").classList.toggle("expand");
 });
+
+$(".carousel-control-next").on("click", function () {
+  if (scrollPosition < (carouselWidth - cardWidth * 4)) { //check if you can go any further
+    scrollPosition += cardWidth;  //update scroll position
+    $(".carousel-inner").animate({ scrollLeft: scrollPosition },600); //scroll left
+  }
+});
+
+$(".carousel-control-prev").on("click", function () {
+  if (scrollPosition > 0) {
+    scrollPosition -= cardWidth;
+    $(".carousel-inner").animate(
+      { scrollLeft: scrollPosition },
+      600
+    );
+  }
+});
+
+var multipleCardCarousel = document.querySelector(
+  "#carouselExampleControls"
+);
+if (window.matchMedia("(min-width: 768px)").matches) {
+  //rest of the code
+  var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+    interval: false,
+    wrap: false
+  });
+} else {
+  $(multipleCardCarousel).addClass("slide");
+}
+

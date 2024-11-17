456f6f895b7dae4707051a6add0680020de1a45b
KanyantaM <kanyanta.1makasa@gmail.com>
2024-11-17 15:02:53 +0200

    adding github to docs

--- reviza-doc/docusaurus.config.ts
+++ reviza-doc/docusaurus.config.ts
@@ -131,7 +131,7 @@ const config: Config = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/KanyantaM/reviza',
             },
           ],
         },

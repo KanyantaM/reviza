4b6b25e0f85219b1d973138a5832498c47af4640
KanyantaM <kanyanta.1makasa@gmail.com>
2024-11-22 14:44:56 +0200

    updating packages

--- reviza-doc/docusaurus.config.ts
+++ reviza-doc/docusaurus.config.ts
@@ -1,7 +1,6 @@
 import { themes as prismThemes } from "prism-react-renderer";
 import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
-import useBaseUrl from "@docusaurus/useBaseUrl";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
@@ -30,7 +29,7 @@ const config: Config = {
   // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "en",
-    locales: ["en"],
+    // locales: ["en"],
   },
 
   presets: [
